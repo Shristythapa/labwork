35d91f5a069e92324a3915fda1c7b989005d9bbe
Shristy <thapashristy110@gmail.com>
2021-11-30 18:32:00 +0545

    labwork assignment

---
+++ lab1.py
@@ -0,0 +1,6 @@
+
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+c = int(input("enter the third number"))
+sum = a+b+c
+print(f"the sum of the numbers is {sum }:")

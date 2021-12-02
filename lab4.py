9c04b6c316a744dfa9431f0a7a513df4267b07f7
Shristy <thapashristy110@gmail.com>
2021-12-02 12:45:19 +0545

    error correction

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
 #sum fo first ten positive number
 n = [1,2,3,4,5,6,7,8,9,10,11]
-sum = (n * (n+1)) / 2
+sum = sum(n)
 print("the sum of the first ten positive number is {}".format(sum) )
--- lab6.py
+++ lab6.py
@@ -1,4 +1,4 @@
 #sum of first even number
 n = [2,4,6,8,10,12,14,16,18,20]
-sum = (n*(n+1))/2
+sum = sum(n)
 print ("sum of first ten even is {}".format(sum))
